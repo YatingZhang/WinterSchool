c1f65980b68c7bb3b09df9c130dae5d21de1b1fc
YatingZhang <mzwaverly@gmail.com>
2014-11-06 20:47:43 +0800

    user register example

--- WinterSchool/urls.py
+++ WinterSchool/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from winSchool.views import register
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,5 +10,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^WinterSchool/register/', 'winSchool.views.register'),
     url(r'^WinterSchool/', 'winSchool.views.home'),
+    
 )
--- winSchool/views.py
+++ winSchool/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from winSchool.models import User
 
 def home(request):
     return render_to_response(
         'index.html', {}, context_instance=RequestContext(request))
+
+def register(request):
+    user_name=request.POST['user_name']
+    password=request.POST['pass']
+    user = User(name=user_name)
+    user.save()
+    return render_to_response(
+        'index.html',{'message':'success'}, context_instance=RequestContext(request))
