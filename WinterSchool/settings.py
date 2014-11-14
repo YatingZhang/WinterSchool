472cf4b56b4d7fd47fe609c564b46e53b67d4997
silent-hill <root@virt0-0-0-5.local>
2014-11-15 02:50:41 +0800

    update WinterSchool

--- WinterSchool/settings.py
+++ WinterSchool/settings.py
@@ -100,7 +100,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_PATH, 'static'),
+    os.path.join(PROJECT_PATH, 'staticfile'),
 )
 
 
