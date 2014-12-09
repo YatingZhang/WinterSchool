a7b6dc57333e3ab5b25b7ad9a39ec9a82fb499cd
YatingZhang <mzwaverly@gmail.com>
2014-12-09 14:20:05 +0800

    Update map.js

--- static/js/map.js
+++ static/js/map.js
@@ -1,13 +1,13 @@
- function initMap(markerArr, id){
-        createMap(id);//创建地图
+ function initMap(markerArr, map_id){
+        createMap(map_id);//创建地图
         setMapEvent();//设置地图事件
         addMapControl();//向地图添加控件
         addMarker(markerArr);//向地图中添加marker
     }
     
     //创建地图函数：
-    function createMap(){
-        var map = new BMap.Map(id);//在百度地图容器中创建一个地图
+    function createMap(map_id){
+        var map = new BMap.Map(map_id);//在百度地图容器中创建一个地图
         var point = new BMap.Point(121.428808,31.02154);//定义一个中心点坐标
         map.centerAndZoom(point,15);//设定地图的中心点和坐标并将地图显示在地图容器中
         window.map = map;//将map变量存储在全局
